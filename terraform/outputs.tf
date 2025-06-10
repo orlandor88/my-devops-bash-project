e9eed9cf014d82404976f5ada53080a48f5b5c7b
Orlando Tudor <eugen_orlando_17@yahoo.com>
2025-06-10 11:54:30 +0300

    Modified key pair4

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,6 @@ output "public_ip" {
 }
 
 output "private_key_pem" {
-  value     = tls_private_key.example.private_key_pem
+  value     = tls_private_key.generated_key.private_key_pem
   sensitive = true
 }
